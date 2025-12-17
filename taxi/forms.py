498bcd7d6420d9c36c5de538afeaab8938ff7569
Alex <alexx5039@gmail.com>
2025-12-17 07:51:04 +0200

    flake fixed

--- taxi/forms.py
+++ taxi/forms.py
@@ -5,7 +5,9 @@ from django import forms
 from .models import Car
 from django.contrib.auth import get_user_model
 
+
 Driver = get_user_model()
+
 
 def validate_license_number(license_number: str):
     if not re.fullmatch(r"^[A-Z]{3}\d{5}$", license_number):
